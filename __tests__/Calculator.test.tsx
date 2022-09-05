a0225a37d1601323f315512dbcbe2b7e3f298bac
Andrei Zhulid <>
2022-09-05 11:27:13 +0200

    Refactored: Extracted common logic to a function

--- __tests__/Calculator.test.tsx
+++ __tests__/Calculator.test.tsx
@@ -1,17 +1,20 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import Calculator from '@/components/Calculator';
 
+const setupInput = () => {
+  render(<Calculator />)
+  return screen.getByTestId('input') as HTMLInputElement
+}
+
 describe('Home', () => {
   it('Calculator renders an empty text input', () => {
-    render(<Calculator />)
-    const input = screen.getByTestId('input') as HTMLInputElement
+    const input = setupInput()
     expect(input).toBeInTheDocument()
     expect(input.value).toBe("")
   })
 
   it('display entered value as roman numeral', () => {
-    render(<Calculator />)
-    const input = screen.getByTestId('input') as HTMLInputElement
+    const input = setupInput()
     fireEvent.change(input, { target: { value: "23" } })
     const romanNumeral = screen.getByTestId('roman-numeral')
     expect(romanNumeral.textContent).toBe("23")
