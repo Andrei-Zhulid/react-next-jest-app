4c880eba61061de00246c53c4971c016f4f0a9d5
Andrei Zhulid <>
2022-09-05 10:09:15 +0200

    Updated the heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '@/pages/index.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to Next.js!</h1>
+      <h1 className={styles.title}>Roman Numeral Calculator</h1>
     </div>
   )
 }
